d7466605a9feb5653a72fe54b07994bbc5fd7638
Quan Pham <doraemonfanclub@gmail.com>
2018-10-16 16:24:20 +0700

    Refactor folder structures

--- android/src/main/java/com/clipsub/RNCountryCodes/RNCountriesModule.java
+++ android/src/main/java/com/clipsub/RNCountryCodes/RNCountriesModule.java
@@ -1,22 +1,22 @@
 
-package com.reactlibrary;
+package com.clipsub.RNCountryCodes;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
-public class RNCountriesModule extends ReactContextBaseJavaModule {
+public class RNCountryCodesModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
 
-  public RNCountriesModule(ReactApplicationContext reactContext) {
+  public RNCountryCodesModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
   }
 
   @Override
   public String getName() {
-    return "RNCountries";
+    return "RNCountryCodes";
   }
 }
--- android/src/main/java/com/clipsub/RNCountryCodes/RNCountriesPackage.java
+++ android/src/main/java/com/clipsub/RNCountryCodes/RNCountriesPackage.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.clipsub.RNCountryCodes;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -9,16 +9,11 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
-public class RNCountriesPackage implements ReactPackage {
+
+public class RNCountryCodesPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNCountriesModule(reactContext));
-    }
-
-    // Deprecated from RN 0.47
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
+      return Arrays.<NativeModule>asList(new RNCountryCodesModule(reactContext));
     }
 
     @Override
